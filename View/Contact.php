ba6e6325fb326deb1b7224df7df2ce2904300673
Jon00312 <35850023+Jon00312@users.noreply.github.com>
2018-10-25 15:44:44 +0200

    menu + contact

---
+++ View/Contact.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../Public/CSS/Contact.css">
+	<link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+	<div id="divlogo">
+		<img src="../Public/Img/logo_movie_night.svg" id="logo">
+	</div>
+	<p id = "titre">Contact</p>
+	<div id="divContenu">
+		<form>
+			<div id="divObjet">
+				<label for="objet">Objet de la demande :</label><br>
+				<input type="text" name="objet" class="textInput"><br>
+				<label for="commentaires">Commentaires :</label><br>
+				<textarea rows="10" name="commentaires" id="commentaires"></textarea><br>
+			</div>
+			<div id="divSubmit">
+				<button type="submit" id="submit_button">
+					<i class="fa fa-play circle"></i>
+				</button>
+			</div>
+		</form>
+	</div>
+</body>
+<script type="text/javascript"></script>
+<?php 
+	include("menu.php");
+?>
+</html>
