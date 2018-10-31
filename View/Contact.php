864200901334b11b2b998a5c5e4ab8cd6d6fc6ff
Jon00312 <35850023+Jon00312@users.noreply.github.com>
2018-10-31 18:33:17 +0100

    debut evenements

--- View/Contact.php
+++ View/Contact.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Contact</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="../Public/CSS/Contact.css">
 	<link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet">
---
+++ View/Evenements.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Evenements</title>	
+	<link rel="stylesheet" type="text/css" href="../Public/CSS/Evenements.css">
+	<link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+</head>
+<body>
+	<div id="contenu">
+		<h1 id="titre">Evenements</h1>
+
+		<section class="special event"></section>
+		<section class="event"></section>
+		<section class="special event"></section>
+		<section class="event"></section>
+		<section class="event"></section>
+	</div>
+
+	<?php
+		include("menu.php");
+	?>
+</body>
+</html>
