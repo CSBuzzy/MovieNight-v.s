f8870020104e73d368ee77bed060b0147fb0c163
Jon <35850023+Jon00312@users.noreply.github.com>
2018-11-16 11:30:12 +0100

    fiche evenement

--- View/Vue_Evenement.php
+++ View/Vue_Evenement.php
@@ -6,6 +6,8 @@
 	<link rel="stylesheet" type="text/css" href="../Public/CSS/Evenement.css">
 	<link href="https://fonts.googleapis.com/css?family=Megrim" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="../Public/JS/evenement.js"></script>
 </head>
 <body>
 	<div id="contenu">
@@ -14,12 +16,22 @@
 		<section id="detailsEvenement">
 			<img id="image" src=<?php print($event->getImage_event()) ?>>
 			<h2 id="titreEvenement"><?php print($event->getName_event()) ?></h2>
-			<label id="langue">VO VF <span id="langueChoisie">VOSTFR</span> VOSTA</label>
-			<label id="contenu"><span class = "sousTitres">Contenu : </span><?php print($contentList[0]->getName_content()) ?></label><br/>
+			<label id="langue">
+				<span class = "languesContenu" data-language = <?php print($contentList[0]->getLanguage()) ?>></span>
+				<!--span class = "languesContenu" data-language = "VOSTA"></span-->
+				<span class = "langue" data-language = "VO">VO </span>
+				<span class = "langue" data-language = "VF">VF </span>
+				<span class = "langue" data-language = "VOSTFR">VOSTFR </span>
+				<span class = "langue" data-language = "VOSTA">VOSTA</span>
+			</label>
+			<label id="contenu"><span class = "sousTitres">Titre(s) : </span><?php print($contentList[0]->getName_content()) ?></label><br/>
 			<label id="genre"><span class = "sousTitres">Genre : </span><?php print($contentList[0]->getGenre()) ?></label><br/>
 			<label id="localisation"><span class = "sousTitres">Localisation : </span><?php print(ucfirst(strtolower($event->getCity()))) ?></label><br/>
-			<label id="date"><span class = "sousTitres">Date : </span><?php print($event->getDate_event()) ?></label><br/>
-			<label id="heure"><span class = "sousTitres">Heure : </span><?php print($event->getDuration_event()) ?></label><br/>
+			<label id="date"><span class = "sousTitres">Date : </span>
+				<?php setlocale(LC_TIME, 'fra_fra'); print(strftime('%A %d %B %G',(strtotime($event->getDate_event())))) ?>
+			</label><br/>
+			<label id="heure"><span class = "sousTitres">Heure : </span>
+				<?php print(strftime('%R',(strtotime($event->getDate_event())))) ?></label><br/>
 			<label id="description"><span class = "sousTitres">Description : </span><div id="texteDescription"><?php print($event->getDescription_event()) ?></div></label><br/>
 			<label id="organisateur"><span class = "sousTitres">Organisateur : </span><?php print($event->getOrganizer()) ?></label><br/>
 			<label id="preferences"><span class = "sousTitres">Préférences : </span>//TODO</label><br/>
@@ -28,6 +40,7 @@
 			<a href="../Controller/Evenements.php"><i class="fa fa-fast-backward circle"></i></a>
 			<a href="../Controller/Evenements.php"><img id="ticket" src="../Public/Img/ticket.svg"></a>
 		</section>
+		
 
 
 	</div>
--- View/Vue_Liste_Evenements.php
+++ View/Vue_Liste_Evenements.php
@@ -11,7 +11,6 @@
 		<h1 id="titre">Evenements</h1>
 
 		<?php  
-
 			foreach ($eventList as $event ) {
 
 				if ($event->getSpecial_event() == 0) {
@@ -20,66 +19,20 @@
 				else {
 					$script = '<section class="special event">';
 				}
-
+				setlocale(LC_TIME, 'fra_fra');
 				$script .= '<img id="image" src="'.$event->getImage_event().'">';
 				$script .= '<div id="texteEvenement">';
 				$script .= '<h2 id="titreEvenement">'.$event->getName_event().'</h2>';
 				$script .= '<strong>Titre(s) :</strong> "//TODO"<br/>';
-				$script .= '<strong>Date :</strong> '.$event->getDate_event().' <br/>';
-				$script .= '<strong>Heure :</strong> '.$event->getDuration_event().'<br/>';
+				$script .= '<strong>Date :</strong> '. strftime('%A %d %B',(strtotime($event->getDate_event()))).' <br/>';
+				$script .= '<strong>Heure :</strong> '.strftime('%R',(strtotime($event->getDate_event()))).'<br/>';
 				$script .= '<strong>Ville : </strong><span id = "ville">'.ucfirst(strtolower($event->getCity())).'</span><br/>';
 				$script .= '<strong>Places disponibles :</strong> '.$event->getSeatsRemaining_event().'/'.$event->getMember_number_event().'<br/>';
 				$script .= '<a class="lienEvenement" href="Evenements.php?event='.$event->getId_event().'"><i class="fa fa-play circle"></i></a>';
 				$script .= '</div></section>';
 				print($script);
-
 			}
-
 		?>
-
-		<section class="special event">
-			<img id="image" src="../Public/Img/image.png">
-			<div id="texteEvenement">
-					<h2 id="titreEvenement">Harry Potter</h2>
-					<span class="id_event">id_event</span>
-					<strong>Titre :</strong> "Les reliques de la mort part 2"<br/>
-					<strong>Date :</strong> Jeudi 1 Novembre <br/>
-					<strong>Heure :</strong> 21h<br/>
-					<strong>Ville :</strong> Cenon<br/>
-					<strong>Places disponibles :</strong> 6/6<br/>
-					<a class="lienEvenement" href="Evenements.php?event=id_event"><i class="fa fa-play circle"></i></a>
-			</div>
-		</section>
-		<section class="event">
-			<img id="image" src="../Public/Img/image.png">
-			<div id="texteEvenement">
-					<h2 id="titreEvenement">Harry Potter</h2>
-					<span class="id_event">id_event</span>
-					<strong>Titre :</strong> "L'école des sorciers"<br/>
-					<strong>Date :</strong> Jeudi 1 Novembre <br/>
-					<strong>Heure :</strong> 21h<br/>
-					<strong>Ville :</strong> Cenon<br/>
-					<strong>Places disponibles :</strong> 3/6<br/>
-					<a class="lienEvenement" href="Evenements.php?event=id_event"><i class="fa fa-play circle"></i></a>
-			</div>
-		</section>
-		<section class="special event"></section>
-		<section class="event">
-			<img id="image" src="../Public/Img/image.png">
-			<div id="texteEvenement">
-					<h2 id="titreEvenement">Harry Potter</h2>
-					<span class="id_event">id_event</span>
-					<strong>Titre :</strong> Les reliques de la mort part2<br/>
-					<strong>Date :</strong> Jeudi 1 Novembre <br/>
-					<strong>Heure :</strong> 21h<br/>
-					<strong>Ville :</strong> Cenon<br/>
-					<strong>Places disponibles :</strong> 0/6<br/>
-					<a class="lienEvenement" href="Evenements.php?event=id_event"><i class="fa fa-play circle"></i></a>
-			</div>
-		</section>
-		<section class="event"></section>
-	</div>
-
 	<?php
 		include("menu.php");
 	?>
